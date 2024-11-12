9a09cfa99de86129c2cb95d3c20a38a4b1d47791
Robbie Gill <robbiegill@U.boisestate.edu>
2024-11-11 20:00:10 -0700

    asdf

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -73,8 +73,8 @@ public class HerokuApplication {
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
       stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
       //ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM table_timestamp_and_random_string");
-
+      //ResultSet rs = stmt.executeQuery("SELECT tick FROM table_timestamp_and_random_string");
+      ResultSet rs = stmt.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
