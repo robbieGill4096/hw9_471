1edef5eb88e221d9bc63816f00549713ae806991
Robbie Gill <robbiegill@U.boisestate.edu>
2024-11-11 19:25:41 -0700

    troubleshooting randomstringmethod

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -76,7 +76,7 @@ public class HerokuApplication {
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick"));
-        System.out.println("Robbie Gill");
+        //System.out.println("Robbie Gill");
       }
 
       model.put("records", output);
