0d1eb6434d06da0b35a8201b5239a8e08bb4ae29
Robbie Gill <robbiegill@U.boisestate.edu>
2024-11-11 19:56:15 -0700

    asdf

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -77,7 +77,7 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
         System.out.println("Robbie Gill");
       }
 
