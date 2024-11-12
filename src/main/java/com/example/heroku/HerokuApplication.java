e019393ae1961ad5f405d19c6fb5192baf79c9cf
Robbie Gill <robbiegill@U.boisestate.edu>
2024-11-11 19:18:37 -0700

    made database changes a randomstring and the print stmt

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -52,18 +52,31 @@ public class HerokuApplication {
   String index() {
     return "index";
   }
-
+  public String getRandomString() {
+    int length = 20; // You can adjust the desired string length here
+    StringBuilder sb = new StringBuilder();
+    String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    Random random = new Random();
+    for (int i = 0; i   
+   < length; i++) {
+      int index = random.nextInt(characters.length());
+      sb.append(characters.charAt(index));
+    }
+    return sb.toString();   
+  
+  }
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick"));
+        System.out.println("Robbie Gill");
       }
 
       model.put("records", output);
