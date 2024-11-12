71612126c7e67b0c55f63d6a86c7007a211210e8
Robbie Gill <robbiegill@U.boisestate.edu>
2024-11-11 19:32:29 -0700

    troubleshooting randomstringmethod

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -56,8 +56,8 @@ public class HerokuApplication {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final SecureRandom RANDOM = new SecureRandom();
 
-    public static String generateRandomString(int length) {
-        StringBuilder sb = new StringBuilder(length);
+    public static String generateRandomString() {
+        StringBuilder sb = new StringBuilder(15);
         for (int i = 0; i < length; i++) {
             int index = RANDOM.nextInt(CHARACTERS.length());
             sb.append(CHARACTERS.charAt(index));
